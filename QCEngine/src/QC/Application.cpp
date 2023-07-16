8f1dd0d67b5013f0952505c4b2e99625fb931992
CyFox <91355011+MRogers20@users.noreply.github.com>
2023-07-16 11:41:06 -0500

    GLFW window events (unfinished)

--- QCEngine/src/QC/Application.cpp
+++ QCEngine/src/QC/Application.cpp
@@ -3,11 +3,16 @@
 #include "QC/Events/ApplicationEvent.h"
 #include "QC/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace QC
 {
+#define BIND_EVENT_FN(x) std::(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
@@ -15,10 +20,17 @@ namespace QC
 
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		QC_CORE_INFO("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		while (m_Running)
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	}
--- QCEngine/src/QC/Application.h
+++ QCEngine/src/QC/Application.h
@@ -13,6 +13,7 @@ namespace QC
 		virtual ~Application();
 
 		void Run();
+		void OnEvent(Event& e);
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
